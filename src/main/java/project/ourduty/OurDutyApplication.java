56594fcb11594baad9b155950b2bfe81e4447919
Jaeuk Oh <wooko5@naver.com>
2024-03-07 15:52:34 +0900

    init project

---
+++ src/main/java/project/ourduty/OurDutyApplication.java
@@ -0,0 +1,13 @@
+package project.ourduty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurDutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurDutyApplication.class, args);
+	}
+
+}
